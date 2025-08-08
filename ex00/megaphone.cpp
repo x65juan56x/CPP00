568f2d0ffe34425e71a88147f13a922d1e401ba4
x65juan56 <x6juan6x@gmail.com>
2025-08-08 16:22:22 +0200

    ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,9 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmondon <jmondon@student.42malaga.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/08 15:55:42 by jmondon           #+#    #+#             */
+/*   Updated: 2025/08/08 16:09:31 by jmondon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
 int	main(int ac, char **av)
 {
+	int i = 1;
+	int j;
+
 	if (ac == 1)
-		/*imprime "* LOUD AND UNBEARABLE FEEDBACK NOISE *"*/
-	while (/*args*/)
-	{
-		/*imprimir argumentos en uppercase*/
-	}
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+		while (i < ac)
+		{
+			j = 0;
+			while (av[i][j])
+			{
+				std::cout << (char)std::toupper(av[i][j]);
+				j++;
+			}
+			i++;
+		}
+	std::cout << std::endl;
+	return (0);
 }
